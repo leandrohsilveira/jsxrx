0fc86106bfba286944f0fcf836cdfa3440751a82
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2026-06-29 00:29:04 -0300

    Add analyze bundle vite plugin

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite"
 import { jsxRX } from "@jsxrx/vite-plugin"
+import { analyzer } from "vite-bundle-analyzer"
+import { env } from "node:process"
 
-export default defineConfig({
-  plugins: [jsxRX()],
-})
+export default defineConfig(() => ({
+  plugins: [
+    jsxRX(),
+    analyzer({ enabled: env.VITE_PLUGIN_ENABLE_ANALYZER === "true" }),
+  ],
+}))
